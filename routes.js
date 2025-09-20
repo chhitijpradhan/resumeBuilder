9122505cb9dfcc0078f9076d3b8f5ac9a61f06e1
chhitijpradhan <chhitijpradhan13@gmail.com>
2025-09-20 07:37:05 +0530

    resmoved some stuff

--- routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const router = express.Router();
-router.get("./get-templetes", getTempletes);
-router.post("/login", userLogin);
-router.post("/register", userRegister);
-module.export = router;
